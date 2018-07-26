868bec638e5bc15a86b58781045293b089d707f7
Paul Grayson <pdg@alum.mit.edu>
2018-07-25 21:53:29 -0700

    wrote options

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,11 +1,22 @@
 class Hangman
-  def filter_words(words, pattern, missing_letters)
-    char_class = if missing_letters.empty?
+  def filter_words(words, pattern, used_letters)
+    char_class = if used_letters.empty?
                    '.'
                  else
-                   '[^'+missing_letters.join+']'
+                   '[^'+used_letters.join+']'
                  end
     regexp = Regexp.new(pattern.gsub('_',char_class))
     words.grep regexp
+  end
+
+  def options(pattern, guess)
+    [pattern] +
+      pattern.length.times.collect do |i|
+      if pattern[i] == '_'
+        pattern.slice(0,i) + guess + pattern.slice(i+1,pattern.length-1)
+      else
+        nil
+      end
+    end.compact
   end
 end
--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -3,6 +3,21 @@ require_relative '../lib/hangman.rb'
 describe Hangman do
   subject { Hangman.new }
 
+  describe '#options' do
+    it 'understands the one-letter case' do
+      expect(subject.options('_','a')).to eq %w(_ a)
+    end
+
+    it 'understands the two-letter case' do
+      expect(subject.options('__','a')).to eq %w(__ a_ _a)
+    end
+
+    it 'understands the three-letter case' do
+      expect(subject.options('___','a')).to eq %w(___ a__ _a_ __a)
+      expect(subject.options('_c_','a')).to eq %w(_c_ ac_ _ca)
+    end
+  end
+
   describe '#filter_words' do
     context 'given an empty list' do
       let(:list) { [] }
